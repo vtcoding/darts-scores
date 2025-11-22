45a3ef1b2e1ddcf8378957544b41d9980f9c0b82
unknown <toikkanenville@gmail.com>
2025-11-22 19:48:54 +0200

    Added UI for dropdown

--- src/components/HitRates/HitRates.tsx
+++ src/components/HitRates/HitRates.tsx
@@ -5,6 +5,9 @@ import { getTotalHitRatesForSector } from "../../utils";
 import Block from "../Block/Block";
 import Title from "../Title/Title";
 import styles from "./HitRates.module.css";
+import Dropdown from "../Dropdown/Dropdown";
+import { useState } from "react";
+import BlockParagraph from "../BlockParagraph/BlockParagraph";
 
 interface HitRatesProps {
   mode: string;
@@ -13,13 +16,38 @@ interface HitRatesProps {
 
 const HitRates = ({ mode, practiceMatches }: HitRatesProps) => {
   const { t } = useTranslation();
+  const [selectedOrder, setSelectedOrder] = useState<string>("sector");
   const filteredMatches = practiceMatches.filter((match) => match.mode === mode);
   const finish_on = mode === "around-the-clock" ? 25 : 50;
+  const orderedHitRates = () => {
+    const rates: number[] = [];
+    [...Array(20)].forEach((_, i) => {
+      const rate = getTotalHitRatesForSector(filteredMatches, i + 1);
+      rates.push(rate);
+    });
+  }
+  
+  const orderOptions = [
+    {
+      name: "Sector",
+      id: "sector"  
+    },
+    {
+      name: "Hit rate",
+      id: "rate"
+    }
+  ];
 
   return (
     <Block>
       <div className={styles.hitRates}>
         <Title text={t("components.hitRates.title")} />
+        <div className={styles.orderBy}>
+        <BlockParagraph>
+          Order by:
+        </BlockParagraph>
+        <Dropdown options={orderOptions} selectedOption={selectedOrder} setSelectedOption={setSelectedOrder}/>
+        </div>
         {practiceMatches.length > 0 && (
           <div className={styles.columns}>
             <div className={styles.column}>
